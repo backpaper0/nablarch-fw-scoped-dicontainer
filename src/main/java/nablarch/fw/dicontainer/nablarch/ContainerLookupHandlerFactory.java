1e9ec1c23ea2fa404dfbe00b35679c590252cc97
Tsuyoshi Kawasaki <kawasaki.tsuyoshi@tis.co.jp>
2019-10-07 14:40:09 +0900

    5u16-SNAPSHOTを使用.

--- src/main/java/nablarch/fw/dicontainer/nablarch/ContainerLookupHandlerFactory.java
+++ src/main/java/nablarch/fw/dicontainer/nablarch/ContainerLookupHandlerFactory.java
@@ -1,13 +1,12 @@
 package nablarch.fw.dicontainer.nablarch;
 
 import nablarch.fw.dicontainer.Container;
-import nablarch.fw.handler.HandlerFactory;
+import nablarch.fw.handler.DelegateFactory;
 
-public final class ContainerLookupHandlerFactory implements HandlerFactory {
+public final class ContainerLookupHandlerFactory implements DelegateFactory {
 
     @Override
-    public Object create(final Class<?> clazz)
-            throws InstantiationException, IllegalAccessException {
+    public Object create(final Class<?> clazz) {
         final Container container = ContainerImplementers.get();
         return container.getComponent(clazz);
     }
